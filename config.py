da0e460029cde1c600c778a557a37b6e03b358a0
eralimuhammadiyev28-lgtm <eralimuhammadiyev28@gmail.com>
2026-08-07 17:08:14 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-BOT_TOKEN = "8812612035:AAHniOa5FEOv7oshFIxPBUvYIsYSPHJ8H64"
+BOT_TOKEN = "SIZNING_YANGI_TOKENINGIZ_BU_YERGA"  # @BotFather'dan yangi bot yarating va tokenni shu yerga qo'ying
 CHANNEL_ID = "@Kamal_Tarix"
 CHANNEL_LINK = "https://t.me/Kamal_Tarix"
 ADMIN_IDS = [6375927297]
-BOT_USERNAME = elegant_konkurs_bot  # @BotFather dan olgan username (@ siz)
+BOT_USERNAME = "elegant_konkurs_bot"  # TUZATILDI: qo'shtirnoq qo'shildi
