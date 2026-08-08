1ce3637961c12aae52cb54d20bcb956ff714b2e6
eralimuhammadiyev28-lgtm <eralimuhammadiyev28@gmail.com>
2026-08-08 09:47:39 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
-BOT_TOKEN = "SIZNING_YANGI_TOKENINGIZ_BU_YERGA"  # @BotFather'dan yangi bot yarating va tokenni shu yerga qo'ying
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN", "")  # Railway -> Variables bo'limidan olinadi
 CHANNEL_ID = "@Kamal_Tarix"
 CHANNEL_LINK = "https://t.me/Kamal_Tarix"
 ADMIN_IDS = [6375927297]
-BOT_USERNAME = "elegant_konkurs_bot"  # TUZATILDI: qo'shtirnoq qo'shildi
+BOT_USERNAME = "elegant_konkurs_bot"
